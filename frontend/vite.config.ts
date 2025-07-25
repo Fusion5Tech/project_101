4c5c209f5c1f9f24642c76d673099a8439cfe0b8
sehmaluva <machinguramalvint@gmail.com>
2025-07-25 12:23:28 +0200

    .....vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: '/project101/', 
   plugins: [react()],
-  build: {
-    outDir: 'dist', 
-  },
-  server: {
-    port: 3000,
-    open: true,
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
